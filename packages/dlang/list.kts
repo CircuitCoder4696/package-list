ff6b278ef3abf91b3e9d47c579d494a0383b7ad7
CircuitCoder4696 <50537531+CircuitCoder4696@users.noreply.github.com>
2021-12-17 11:00:30 -0600

    Update list.kts

--- packages/dlang/list.kts
+++ packages/dlang/list.kts
@@ -1,5 +1,5 @@
 fun genPackageList(PackageManager packageManager) {
-  packageManager list repository("cuda-d","https://github.com/prasunanand/cuda_d/archive/refs/heads/master.zip",NoVersionSet)
-  packageManager list repository("vibe-d","https://github.com/vibe-d/vibe.d/archive/refs/heads/master.zip",NoVersionSet)
-  packageManager.otherLang("C","importC") list repository("https://github.com/klassmann/blog-embedding-lua-in-c/archive/refs/heads/master.zip",NoVersionSet)
+  packageManager list repository("cuda-d",NoVersionSet,"https://github.com/prasunanand/cuda_d/archive/refs/heads/master.zip")
+  packageManager list repository("vibe-d",NoVersionSet,"https://github.com/vibe-d/vibe.d/archive/refs/heads/master.zip")
+  packageManager.otherLang("C","importC") list repository("lua-embed","https://github.com/klassmann/blog-embedding-lua-in-c/archive/refs/heads/master.zip",NoVersionSet)
 };
